253000a942743d5dc365ca8664def23a30735c62
Rishabh Roy <rishabh.roy@domo.com>
2024-01-24 19:58:17 +0530

    commit 1

--- js-example/my-app/src/App.js
+++ js-example/my-app/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import data from "../api/items.json"
 
 function App() {
+  const [isCart, setIsCart] = useState(false)
+  const [cartItems, setCartItems] = useState(JSON.parse(data))
+
+
+
+  const renderPage ()=> isCart ?(
+    <div>
+      <button data-testid="cart-button">Show Shopping Items</button>
+      <button data-testid="request-button">Request Page</button>
+
+      <h1 data-testid="heading">Cart Items</h1>
+      <div>
+        <div data-testid={`title-${title}`}></div>
+        <div data-testid={`price-${price}`}></div>
+        <button data-testid={`removeCartButton-${title}`}>Remove From Cart</button>
+      </div>
+    </div>
+  ) : (
+    <div>
+      <button data-testid="cart-button">Show Cart Items</button>
+      <button data-testid="request-button">Request Page</button>
+
+      <h1 data-testid="heading">Shopping Items</h1>
+      <div>
+        <div data-testid={`title-${title}`}></div>
+        <div data-testid={`price-${price}`}></div>
+        <button data-testid={`addCartButton-${title}`}>Add To Cart</button>
+      </div>
+    </div>
+  )
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {renderPage()}
     </div>
   );
 }
